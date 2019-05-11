ba39e0794c7ed5fa6642867ed4d7b19e05fe6e93
sinyoungnam <sinnam90@gmail.com>
2019-05-11 15:16:21 -0400

    updated CSS

--- src/components/ShopClassesComponents/FilterBar.js
+++ src/components/ShopClassesComponents/FilterBar.js
@@ -11,24 +11,48 @@ export default class FilterBar extends Component {
 
   handleClickLocation = () => {
     let locationContent = document.getElementById("location")
+    let priceContent = document.getElementById("price")
+    let gymContent = document.getElementById("gym")
+    let typeContent = document.getElementById("type")
+    priceContent.style.display = "none"
+    gymContent.style.display = "none"
+    typeContent.style.display = "none"
     this.setState ({ location : !this.state.location })
     {this.state.location ? locationContent.style.display = "flex" : locationContent.style.display = "none"}
   }
 
   handleClickPrice = () => {
+    let locationContent = document.getElementById("location")
     let priceContent = document.getElementById("price")
+    let gymContent = document.getElementById("gym")
+    let typeContent = document.getElementById("type")
+    locationContent.style.display = "none"
+    gymContent.style.display = "none"
+    typeContent.style.display = "none"
     this.setState ({ price : !this.state.price })
     {this.state.price ? priceContent.style.display = "flex" : priceContent.style.display = "none"}
   }
 
   handleClickGym = () => {
+    let locationContent = document.getElementById("location")
+    let priceContent = document.getElementById("price")
     let gymContent = document.getElementById("gym")
+    let typeContent = document.getElementById("type")
+    locationContent.style.display = "none"
+    priceContent.style.display = "none"
+    typeContent.style.display = "none"
     this.setState ({ gym : !this.state.gym })
     {this.state.gym ? gymContent.style.display = "flex" : gymContent.style.display = "none"}
   }
 
   handleClickType = () => {
+    let locationContent = document.getElementById("location")
+    let priceContent = document.getElementById("price")
+    let gymContent = document.getElementById("gym")
     let typeContent = document.getElementById("type")
+    locationContent.style.display = "none"
+    priceContent.style.display = "none"
+    gymContent.style.display = "none"
     this.setState ({ type : !this.state.type})
     {this.state.type ? typeContent.style.display = "flex" : typeContent.style.display = "none"}
   }
