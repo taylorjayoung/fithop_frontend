de5b98e7b9dfb742d702cbee8215572eecc2f9a3
Taylor Young <taylorjayoung@gmail.com>
2019-05-27 11:57:23 -0400

    updated navbar

--- src/components/general_components/Home.js
+++ src/components/general_components/Home.js
@@ -5,76 +5,76 @@ import Backdrop from "../Backdrop/Backdrop";
 import "./Home.css";
 // import "./Home2.css";
 
-// export default class Home extends Component {
-//   render() {
-//     return (
-//       <div className="HomeDivWrapper">
-//         <div className="HomeBackgroundLineDiv">
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//           <div className="line" />
-//         </div>
-//
-//         <div className="HomeDiv">
-//           <div className="Div1">
-//             <div className="SolganDiv">
-//               <span>Fitness Classes </span>
-//               <span>For 90% Off </span>
-//             </div>
-//
-//             <div className="SignUpDiv">
-//               <p className="SignUpLink" onClick={this.props.handleClickSignUp}>
-//                 Sign Up Free
-//               </p>
-//             </div>
-//           </div>
-//
-//           <div className="Div2">
-//             <div className="ImageDiv">
-//               <div className="ExploreClassesDiv">
-//                 <p
-//                   className="ExploreClassesLink"
-//                   onClick={this.props.handleClickExplore}
-//                 >
-//                   Explore Classes
-//                 </p>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 export default class Home extends Component {
   render() {
     return (
-      <div className="home">
-        <div class="image--container">
-          <img className="background-img" src="image-1.jpg" />
-          <img className="background-img" src="image-2.jpeg" />
-          <img className="background-img" src="image-3.jpeg" />
-          <img className="background-img" src="image-4.jpeg" />
-          <img className="background-img" src="image-5.jpeg" />
+      <div className="HomeDivWrapper">
+        <div className="HomeBackgroundLineDiv">
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
         </div>
-        <div class="home--container">
-          <div class="home__header">Fitness Classes for 90% Off</div>
-          <div class="home__content">
-            <div className="home--signupLink divLink" onClick={this.props.handleClickSignUp}>Sign Up Free</div>
-            <div className="home--classLink divLink" onClick={this.props.handleClickExplore}>Explore Classes</div>
+
+        <div className="HomeDiv">
+          <div className="Div1">
+            <div className="SolganDiv">
+              <span>Fitness Classes </span>
+              <span>For 90% Off </span>
+            </div>
+
+            <div className="SignUpDiv">
+              <p className="SignUpLink" onClick={this.props.handleClickSignUp}>
+                Sign Up Free
+              </p>
+            </div>
+          </div>
+
+          <div className="Div2">
+            <div className="ImageDiv">
+              <div className="ExploreClassesDiv">
+                <p
+                  className="ExploreClassesLink"
+                  onClick={this.props.handleClickExplore}
+                >
+                  Explore Classes
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
+
+// export default class Home extends Component {
+//   render() {
+//     return (
+//       <div className="home">
+//         <div class="image--container">
+//           <img className="background-img" src="image-1.jpg" />
+//           <img className="background-img" src="image-2.jpeg" />
+//           <img className="background-img" src="image-3.jpeg" />
+//           <img className="background-img" src="image-4.jpeg" />
+//           <img className="background-img" src="image-5.jpeg" />
+//         </div>
+//         <div class="home--container">
+//           <div class="home__header">Fitness Classes for 90% Off</div>
+//           <div class="home__content">
+//             <div className="home--signupLink divLink" onClick={this.props.handleClickSignUp}>Sign Up Free</div>
+//             <div className="home--classLink divLink" onClick={this.props.handleClickExplore}>Explore Classes</div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
